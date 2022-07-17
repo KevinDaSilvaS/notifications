fc3f21a6f370e2ac425002d3db3fb61dcb7551ec
Your Name <kevin.kds80@gmail.com>
2022-07-17 00:00:53 +0000

    add underscore to opts

--- lib/notifications_web/controllers/notifications_controller.ex
+++ lib/notifications_web/controllers/notifications_controller.ex
@@ -1,7 +1,7 @@
 defmodule NotificationsWeb.NotificationsController do
   use NotificationsWeb, :controller
 
-  def get_notifications(conn, opts) do
+  def get_notifications(conn, _opts) do
     conn |> put_status(200) |> json(%{:hello => "World"})
   end
 end
