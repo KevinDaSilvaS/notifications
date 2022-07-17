42dac6f970f629082a4f9c738ce1f30998516b15
Your Name <kevin.kds80@gmail.com>
2022-07-17 20:40:39 +0000

    add notifications channel files

---
+++ lib/notifications_web/channels/notifications_channel.ex
@@ -0,0 +1,34 @@
+defmodule NotificationsWeb.NotificationsChannel do
+  use NotificationsWeb, :channel
+
+  @impl true
+  def join("notifications:"<> _topic_name, _payload, socket) do
+      {:ok, socket}
+  end
+
+  # Channels can be used in a request/response fashion
+  # by sending replies to requests from the client
+  @impl true
+  def handle_in("ping", payload, socket) do
+    {:reply, {:ok, payload}, socket}
+  end
+
+  # It is also common to receive messages from the client and
+  # broadcast to everyone in the current topic (notifications:topic_name).
+  @impl true
+  def handle_in("shout", payload, socket) do
+    broadcasting_key = Map.get(payload, "broadcasting_key")
+
+    if authorized?(broadcasting_key) do
+      body = Map.get(payload, "body")
+      broadcast(socket, "shout", body)
+    end
+    {:noreply, socket}
+  end
+
+  # Checks if the broadcaster is authorized.
+  defp authorized?(broadcasting_key) when broadcasting_key == "admin" do
+    true
+  end
+  defp authorized?(_), do: false
+end
