785e0a122926a26992894ca06a4eae3c407ea182
Your Name <kevin.kds80@gmail.com>
2022-07-23 20:13:28 +0000

    create repository for notifications

--- lib/repository/repository.ex
+++ lib/repository/repository.ex
@@ -1,3 +1,33 @@
-defmodule Repository do
+defmodule Repository.Notifications do
+  @behaviour Couchdb
 
+  use Agent
+  @instance_name Couchdb.Notifications
+  def start_link(_) do
+    index = %{
+      :fields     => ["created_date"],
+      :index_name => "date-index",
+      :index_type => "json"
+    }
+
+    config = %{
+      :username => "user",
+      :password => "12345",
+      :db_name  => "notifications",
+      :host     => "172.17.0.4",
+      :port     => 5984,
+      :name     => @instance_name,
+      :index    => index
+    }
+    Agent.start_link(fn -> [] end)
+    Couchdb.start_link(config)
+  end
+
+  def find_notifications(fields, selector, sort, limit, page) do
+    Couchdb.find(fields, selector, sort, {limit, page}, @instance_name)
+  end
+
+  def insert_notifications(notifications) do
+    Couchdb.insert_many(notifications, @instance_name)
+  end
 end
