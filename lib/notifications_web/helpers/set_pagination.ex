a3a6a7ff0bb562e3286d4dd4d222d246a8d94f6b
Your Name <kevin.kds80@gmail.com>
2022-07-23 20:16:36 +0000

    create pagination helper

---
+++ lib/notifications_web/helpers/set_pagination.ex
@@ -0,0 +1,18 @@
+defmodule NotificationsWeb.Helpers.SetPagination do
+  @default_page "1"
+  @default_limit "10"
+
+  def set(opts) do
+    limit = Map.get(opts, "limit", @default_limit) |> String.to_integer()
+    page  = Map.get(opts, "page", @default_page)
+                |> String.to_integer()
+                |> set_page()
+
+    {limit, page}
+  end
+
+  def set_page(page) when page > 0 do
+    page
+  end
+  def set_page(_), do: @default_page |> String.to_integer()
+end
