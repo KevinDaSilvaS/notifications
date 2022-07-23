9f67fbc93d925e05611e619734d9aed254520ca6
Your Name <kevin.kds80@gmail.com>
2022-07-23 20:18:06 +0000

    add repository notifications call to query results

--- lib/notifications_web/controllers/notifications_controller.ex
+++ lib/notifications_web/controllers/notifications_controller.ex
@@ -1,10 +1,19 @@
 defmodule NotificationsWeb.NotificationsController do
   alias Services.NotificationsService
   use NotificationsWeb, :controller
+  alias NotificationsWeb.Helpers.SetPagination
 
-  def get_notifications(conn, _opts) do
-    func = fn -> %{:hello => "World"} end
-    body = NotificationsService.get_notifications(func)
+  def get_notifications(conn, opts) do
+    {limit, page} = SetPagination.set(opts)
+    topic = Map.get(opts, "topic")
+
+    repository = fn ->
+      Repository.Notifications.find_notifications(
+        ["topic", "title", "message", "redirect", "created_date"],
+        %{"topic" => topic}, [%{"created_date" => "desc"}], limit, page)
+    end
+
+    body = NotificationsService.get_notifications(repository)
     conn |> put_status(200) |> json(body)
   end
 end
