8597d86b49a476d8c9e7746b959aa71820be36c2
Your Name <kevin.kds80@gmail.com>
2022-07-25 22:34:54 +0000

    add Couchdb tests

--- test/repository/couchdb/couchdb_test.exs
+++ test/repository/couchdb/couchdb_test.exs
@@ -5,11 +5,11 @@ defmodule Couchdb.CouchdbTest do
 
   defmodule HttpClientMockSuccess do
     def post(_url, _body) do
-      """
+      %{body: """
       {\"docs\": [
 
       ]}
-      """
+      """}
     end
 
     def put(_) do
@@ -23,29 +23,121 @@ defmodule Couchdb.CouchdbTest do
 
   defmodule HttpClientMockInsertManySuccess do
     def post(_url, _body) do
-        "[] "
+      %{body: "[] "}
+    end
+
+    def put(_) do
+      HttpClientMockSuccess.put(:call)
     end
   end
 
-  defmodule HttpClientMockInsertManySuccess do
+  defmodule HttpClientMockError do
     def post(_url, _body) do
-      """
+      %{body: """
       {\"error\": \"reason\"}
-      """
+      """}
+    end
+
+    def put(_) do
+      HttpClientMockSuccess.put(:call)
     end
   end
 
-  test "should call start_link successfully and start Couchdb instance" do
-    config = %{
-      :username => "user",
-      :password => "12345",
-      :host => "172.17.0.4",
-      :db_name => "test_start_link",
-      :http_client => HttpClientMockSuccess,
-      :name => Test.StartLink
-    }
+  describe "start_link" do
+    test "should call start_link successfully and start Couchdb instance" do
+      config = %{
+        :username => "user",
+        :password => "12345",
+        :host => "172.17.0.4",
+        :db_name => "test_start_link",
+        :http_client => HttpClientMockSuccess,
+        :name => Test.StartLink
+      }
 
-    {:ok, _} = Couchdb.start_link(config)
-    assert true
+      {:ok, _} = Couchdb.start_link(config)
+      assert true
+    end
+  end
+
+  describe "find" do
+    test "should call find successfully and list all registers" do
+      config = %{
+        :username => "user",
+        :password => "12345",
+        :host => "172.17.0.4",
+        :db_name => "test_find",
+        :http_client => HttpClientMockSuccess,
+        :name => Test.Find
+      }
+
+      {:ok, _} = Couchdb.start_link(config)
+      query = Couchdb.find([], %{}, %{}, {10, 1}, Test.Find)
+      assert query == {:ok, []}
+    end
+
+    test "should call find when an error occurs" do
+      config = %{
+        :username => "user",
+        :password => "12345",
+        :host => "172.17.0.4",
+        :db_name => "test_find",
+        :http_client => HttpClientMockError,
+        :name => Test.FindError
+      }
+
+      {:ok, _} = Couchdb.start_link(config)
+      query = Couchdb.find([], %{}, %{}, {10, 1}, Test.FindError)
+      assert query == {:error, %{"error" => "reason"}}
+    end
+  end
+
+  describe "insert_many" do
+    test "should call insert_many successfully and insert all registers" do
+      config = %{
+        :username => "user",
+        :password => "12345",
+        :host => "172.17.0.4",
+        :db_name => "test_insert_many",
+        :http_client => HttpClientMockInsertManySuccess,
+        :name => Test.InsertMany
+      }
+
+      {:ok, _} = Couchdb.start_link(config)
+      query = Couchdb.insert_many([], Test.InsertMany)
+      assert query == {:ok, []}
+    end
+
+    test "should call insert_many when an error occurs" do
+      config = %{
+        :username => "user",
+        :password => "12345",
+        :host => "172.17.0.4",
+        :db_name => "test_insert_many",
+        :http_client => HttpClientMockError,
+        :name => Test.InsertManyError
+      }
+
+      {:ok, _} = Couchdb.start_link(config)
+      query = Couchdb.insert_many([], Test.InsertManyError)
+      assert query == {:error, %{"error" => "reason"}}
+    end
+  end
+
+  describe "get_config" do
+    test "should call get_config successfully and start Couchdb instance" do
+      config = %{
+        :username => "user",
+        :password => "12345",
+        :host => "172.17.0.4",
+        :db_name => "test_get_config",
+        :http_client => HttpClientMockSuccess,
+        :name => Test.GetConfig
+      }
+      {:ok, _} = Couchdb.start_link(config)
+
+      expected_keys = [:couchdb_url | Map.keys(config)]
+      config_keys = Couchdb.get_config(Test.GetConfig) |> Map.keys()
+      assert expected_keys == config_keys
+    end
   end
 end
